5fca607e239b1be9d39e5c38f97c6aa861a61cf1
natanimabesha <natanimabesha@www.zhewutech.com>
2022-11-07 12:45:26 +0300

    Add reducers to remove and add new bookks

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,19 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <form>
-    <h1 className="form-header">Add New Book</h1>
-    <div className="form-control-container">
-      <input className="form-input" type="text" placeholder="Book title" />
-      <select className="form-select">
-        <option>Action</option>
-        <option>Drama</option>
-        <option>Adventure</option>
-      </select>
-      <button type="submit" className="form-btn">ADD BOOK</button>
-    </div>
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [inputText, setInputText] = useState({
+    title: '',
+    author: '',
+    category: '',
+  });
 
-  </form>
-);
+  const changeHandler = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title: inputText.title,
+      author: inputText.author,
+      category: inputText.category,
+    };
+    dispatch(addBook(newBook));
+    setInputText({
+      title: '',
+      author: '',
+      category: '',
+    });
+  };
+
+  return (
+
+    <form onSubmit={submitHandler}>
+      <h1 className="form-header">Add New Book</h1>
+      <div className="form-control-container">
+
+        <input
+          key="title-input"
+          name='title'
+          value={inputText.title}
+          onChange={changeHandler}
+          className="form-input"
+          type="text"
+          placeholder="Book title"
+        />
+
+        <input
+          type="text"
+          name="author"
+          key="author-input"
+          placeholder="Book author"
+          value={inputText.author}
+          onChange={changeHandler}
+        />
+
+        <select
+          name="category"
+          key="book-category"
+          onChange={changeHandler}
+          value={inputText.category}
+          className="form-select"
+        >
+          <option value="" hidden>Category</option>
+          <option value="Action">Action</option>
+          <option name="category">Fiction</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Inspiration">Inspiration</option>
+          <option value="Business">Business</option>
+        </select>
+        <button type="submit" className="form-btn">ADD BOOK</button>
+      </div>
+
+    </form>
+  );
+};
 
 export default AddBook;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-const BooksList = (props) => {
-  const { BooksList } = props;
-  const bookList = BooksList.map((book) => (
+const BooksList = () => {
+  const bookState = useSelector((state) => state.booksReducer);
+  const dispatch = useDispatch();
+  const removeHandler = (e) => {
+    console.log(e)
+    dispatch(removeBook(e.target.id));
+  };
+
+  const bookList = bookState.map((book) => (
     <div className="book-list" key={book.id}>
       <div className="book-info">
         <span className="book-genre">{book.genre}</span>
         <h3 className="book-name">{book.name}</h3>
         <span>{book.author}</span>
         <div className="button-container">
-          <button type="submit" className="button-item">Comments</button>
-          <button type="submit" className="button-item">Remove</button>
-          <button type="submit" className="button-item">Edit</button>
+          <button id={book.id} type="button" className="button-item">Comments</button>
+          <button onClick={removeHandler} id={book.id} type="button" className="button-item">Remove</button>
+          <button  id={book.id} type="button" className="button-item">Edit</button>
         </div>
       </div>
       <p>{book.complete}</p>
       <div className="book-chapter-info">
         <span className="current-chapter-header">CURRENT CHAPTER</span>
         <p>{book.currentChapter}</p>
-        <button type="submit" className="btn chapter-btn">UPDATE PROGRESS</button>
+        <button id={book.id} type="button" className="btn chapter-btn">UPDATE PROGRESS</button>
       </div>
     </div>
   ));
