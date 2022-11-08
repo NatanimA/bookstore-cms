f42be68716ec331d56c141cb57320737a0c11b91
natanimabesha <natanimabesha@www.zhewutech.com>
2022-11-08 14:54:58 +0300

    intial use of dispatch and selectors

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookstore-cms/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore-cms/books/REMOVE_BOOK';
 
 const initialState = [
+  {
+    title: 'Game of thrones', author: 'Suad', category: 'Adventure', complete: '60%', currentChapter: 'Chapter:8', id: uuidv4(),
+  },
+  {
+    title: 'Hunger Games', author: 'Hayet', category: 'Romance', complete: '10%', currentChapter: 'Introduction', id: uuidv4(),
+  },
+  {
+    title: 'The Secret', author: 'Natanim', category: 'Drama', complete: '40%', currentChapter: 'Going with the wind', id: uuidv4(),
+  },
+  {
+    title: 'Gone with the wind', author: 'Doni', category: 'Action', complete: '90%', currentChapter: 'Chapter:10', id: uuidv4(),
+  },
 ];
 
 export const addBook = (payload) => ({
